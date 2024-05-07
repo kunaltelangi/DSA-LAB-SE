17f99fcf629a6baed707f8d8d2c8461da3e960dd
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2024-05-07 22:15:17 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,42 +1,6 @@
-#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
-#include<ext/pb_ds/tree_policy.hpp>
-#define gcj "Case #"
-#define adj_list vector<vi>
-#define endl "\n"
-#define spc " "
-#define INF_INT 2e9
-#define INF_LL 2e18
-#define matmax 25
-#define mod 1000000007
-#define mp make_pair
-#define pb push_back
-#define pi pair<int, int>
-#define pip pair<int,pi>
-#define pl pair<ll,ll>
-#define plp pair<ll,pl>
-#define vi vector<int>
-#define vl vector<ll>
-#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define all(x) x.begin(), x.end()
-#define ins insert
-#define lb lower_bound  // First element NOT LESS than val
-#define ub upper_bound  // First element GREATER than val
-#define sz(q) (int)(q.size())
- 
 using namespace std;
-using namespace __gnu_pbds;
 typedef long long int ll;
-typedef long int li;
-
-#define ook order_of_key  // Number of elements STRICTLY smaller than X
-#define fbo find_by_order  //  *ITERATOR* pointing to the kth element (0 order)
-
-#define fo(a,b) for(auto i=a;i<b;++i)
-#define nfo(a,b) for(auto i=a;i>=b;--i)
-#define jfo(a,b) for(auto j=a;j<b;++j)
-#define njfo(a,b) for(auto j=a;j>=b;--j)
-#define tt ll t; cin>>t; while(t--)
 
 const int m = 10;
 void insert(ll x,ll (&h)[m]){
